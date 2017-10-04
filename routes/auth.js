6a3645ba155bf52ffb6d6b2bccb212a0adaf0dee
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-10-04 20:08:59 +0200

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,12 @@ const passport = require('passport');
 router.get('/facebook', passport.authenticate('facebook', { scope: ['public_profile', 'email'] }));
 
 router.get(
-    '/facebook/callback',
+    '/facebook/callback', 
     passport.authenticate('facebook', { failureRedirect: '/' }),
     (req, res) => {
         // Successful authentication, redirect home.
         res.redirect('/dashboard');
-    },
+    }
 );
 
 module.exports = router;
