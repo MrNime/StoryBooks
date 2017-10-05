0cbadb0f33ba56b2ae670909e8e8258c3bf31645
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-10-05 20:00:39 +0200

    ui and add story page

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $('.button-collapse').sideNav();
+    $('select').material_select();
+});
+CKEDITOR.replace('body', {
+    plugins: 'wysiwygarea,toolbar,basicstyles,link'
+});
