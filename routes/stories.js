dca99a06d31ce8e6f13c8641833ae6fba2bf3c7b
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-10-06 16:39:27 +0200

    comments

--- routes/stories.js
+++ routes/stories.js
@@ -22,6 +22,7 @@ router.get('/show/:id', (req, res) => {
         _id: req.params.id
     })
         .populate('user')
+        .populate('comments.commentUser')
         .then((story) => {
             res.render('stories/show', {
                 story
@@ -89,6 +90,27 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     Story.remove({ _id: req.params.id })
         .then(() => res.redirect('/dashboard'));
+});
+
+// Add comment
+router.post('/comment/:id', (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+        .then((story) => {
+            const newComment = {
+                commentBody: req.body.commentBody,
+                commentUser: req.user.id
+            };
+
+            // Add to comments array
+            story.comments.unshift(newComment);
+
+            story.save()
+                .then(story => {
+                    res.redirect(`/stories/show/${story.id}`)
+                });
+        });
 });
 
 module.exports = router;
